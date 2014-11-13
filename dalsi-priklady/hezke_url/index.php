d97eb633496abc2aa9b54b26cc3b6e44894a7318
Martin Dostal - Li5 <martin.dostal@seznam.cz>
2014-11-13 14:23:20 +0100

    Hezké URL adresy. Použijte až v momentě, kdy budete mít hotovou celou aplikaci.

---
+++ dalsi-priklady/hezke_url/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	// POZOR: hezka url do SP pridavejte az na konec. Nejdrive udelejte zakladni aplikaci a az pote ji vylepsujte.
+	
+
+	$q = @$_REQUEST["q"];
+
+	echo "tady mam požadovanou stránku dle hezkých url: $q";
+
+?>
