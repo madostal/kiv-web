5a2936f4b1d279a237035bb8ccb17e4bffd40d93
Martin Dostal - Li5 <martin.dostal@seznam.cz>
2014-11-13 15:04:40 +0100

    opraveny phpWrapper - funguje vždy

--- dalsi-priklady/php_wrapper/php_wrapper.php
+++ dalsi-priklady/php_wrapper/php_wrapper.php
@@ -1,7 +1,8 @@
 <?php
 
+	// varianta1 - pujde ve wampu s php 5.3
 	//interpretem PHP zpracuje obsah promenne
-	function phpWrapper($content) 
+	function phpWrapper2($content) 
 	{
 		ob_start();
 		$content = str_replace('<'.'?php','<'.'?',$content);
@@ -12,19 +13,28 @@
 		return $content;
 	}
 	
-	echo "<br/>Ukázka 1: ";
-	$string = "Hello <?php echo \"World\"; ?> !";
-	$string = phpWrapper($string);
-	echo $string;
+	// univerzalni, funguje vzdy
+	function phpWrapperFromFile($filename)
+	{
+		ob_start();
+		
+		if (file_exists($filename) && !is_dir($filename))
+		{
+			include($filename);
+		}
+		
+		// nacte to z outputu
+		$obsah = ob_get_clean();
+		return $obsah;
+	}
 	
-
 	// nactu php kod
 	$text_do_hlavicky = "abc123";
 	
-	echo "<br/>Ukázka 2: ";
-	$php_kod = file_get_contents("php_kod.inc.php");
-	//$php_kod;
-	$vystup = phpWrapper($php_kod);
+	echo "Ukazka 1:";
+	
+	$filename= "php_kod.inc.php";
+	$vystup = phpWrapperFromFile($filename);
 	echo $vystup;
 	
 ?>
