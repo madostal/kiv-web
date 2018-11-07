3475f21706ea68b7172f8b16cbe571ed9c7179bb
Martin Dostal - Li7-2016 <martin.dostal@seznam.cz>
2018-11-07 17:24:18 +0100

    Web - cv. 6

--- cviceni/dostal/06_cviceni/twig_priklad1/index.php
+++ cviceni/dostal/06_cviceni/twig_priklad1/index.php
@@ -3,6 +3,13 @@
     // hello world
     require_once 'vendor/autoload.php';
 
+    // nebo rucne atd.
+    /*
+    require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+    require_once 'vendor/twig/twig/lib/Twig/ExistsLoaderInterface.php';
+    require_once 'vendor/twig/twig/lib/Twig/LoaderInterface.php';
+    require_once 'vendor/twig/twig/lib/Twig/Loader/Array.php';
+    */
     $loader = new Twig_Loader_Array(array(
         'index' => 'Hello {{ name }}!',
     ));
